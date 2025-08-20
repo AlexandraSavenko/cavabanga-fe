16827bbff5503a9e89b377796a3f2cc792ac386b
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-20 22:55:32 +0300

    footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import css from "./Footer.module.css"
+import React from "react";
+import css from "./Footer.module.css";
+import Logo from "../logo/Logo";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className={css.wrap}>
-      footer
+      <Logo />
+      <p>© 2025 CookingCompanion. All rights reserved.</p>
+      <nav>
+        <NavLink to={"#"}>Recipes</NavLink>
+        <NavLink to={"#"}>Account</NavLink>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,15 @@
 import React, { Suspense } from 'react';
+import Footer from '../footer/Footer';
 // import css from "./Layout.module.css"
 
 const Layout = ({children}) => {
-  return (
+  return ( <div>
     <div className="container">
       <Suspense>
         {children}
       </Suspense>
+    </div>
+    <Footer/>
     </div>
   )
 }
