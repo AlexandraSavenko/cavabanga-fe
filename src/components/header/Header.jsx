910139d062f3016d32fc22562b80186590973fdc
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-21 14:55:50 +0300

    header styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import css from "./Header.module.css"
+import React from "react";
+import css from "./Header.module.css";
+import Logo from "../logo/Logo";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const isLoggedIn = true;
   return (
     <div className={css.wrap}>
-      header
+      <Logo />
+      <nav>
+        <NavLink to={"#"}>Recipes</NavLink>
+        {isLoggedIn ? (
+          <NavLink to={"#"}>My profile</NavLink>
+        ) : (
+          <NavLink to={"#"}>Log in</NavLink>
+        )}
+        {isLoggedIn ? (
+          <NavLink to={"#"}>Add Recipe</NavLink>
+        ) : (
+          <NavLink to={"#"}>Register</NavLink>
+        )}
+      </nav>
+      {isLoggedIn && (
+        <div className={css.userNav}>
+          {" "}
+          <div className={css.userInfo}>
+            {" "}
+            <span className={css.userIcon}>M</span> <p>User</p>
+          </div>{" "}
+          <div className={css.br}></div>
+          <button>
+            <svg className={css.icon}>
+              <use href={`./icons.svg#icon-log-out`}></use>
+            </svg>
+          </button>{" "}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from 'react';
 import Footer from '../footer/Footer';
+import Header from '../header/Header';
 // import css from "./Layout.module.css"
 
 const Layout = ({children}) => {
   return ( <div>
+    <Header/>
     <div className="container">
       <Suspense>
         {children}
