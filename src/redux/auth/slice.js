71e6ae840218dd249c55fa8c09d5c5a3e5a518f6
hanna-uler <hannauler@gmail.com>
2025-08-22 19:14:54 -0400

    upd auth init state, add login extraReducers

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,48 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { register } from "./operations";
-import { ErrorMessage } from "formik";
+import { register, login } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {
     user: {
+      id: null,
       name: null,
       email: null,
-      savedRecipes: []
+      savedRecipes: [],
+      ownRecipes: []
     },
     token: null,
     isLoggedIn: false,
     error: false,
-    // ErrorMessage: ""
+    // errorMessage: ""
   },
   // reducers: {},
   extraReducers: builder =>
     builder
       // .addCase(register.pending, (state, action) => {} OR handlePending)
       .addCase(register.fulfilled, (state, action) => {
+        state.user.id = action.payload.user._id;
         state.user.name = action.payload.user.name;
         state.user.email = action.payload.user.email;
         state.user.savedRecipes = action.payload.user.savedRecipes;
         state.token = action.payload.token;
         state.isLoggedIn = true;
+        state.error = false;
       })
       // .addCase(register.rejected, (state, action) => { } OR handleError)
-      // .addCase(login.fulfilled, (state, action) => {
-      //   state.isLoggedIn = true;
-      //   state.user = action.payload.user;
-      //   state.token = action.payload.token;
-      // })
+      .addCase(login.fulfilled, (state, action) => {
+        state.user.id = action.payload.user._id;
+        state.user.name = action.payload.user.name;
+        state.user.email = action.payload.user.email;
+        state.user.savedRecipes = action.payload.user.savedRecipes;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.error = false;
+      })
 //       .addCase(login.rejected, handleError)
 //       .addCase(logout.pending, handlePending)
-//       .addCase(logout.fulfilled, (state) => {
-//         state.isLoggedIn = false;
-//         state.user = {
-//           name: null,
-//           email: null,
-//         };
-//         state.token = null;
-//       })
-//   }
+      // .addCase(logout.fulfilled, (state) => {
+      //   state.isLoggedIn = false;
+      //   state.user = {
+      //     name: null,
+      //     email: null,
+      //   };
+      //   state.token = null;
+      // })
+      // .addCase(login.rejected, handleError)
+
 });
 
   export default authSlice.reducer;
