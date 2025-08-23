da6520215f69ca3543ca61177cb3f1d990350247
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-23 20:47:00 +0300

    modal added

---
+++ src/components/modalNotAutor/ModalNotAutor.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import css from "./ModalNotAutor.module.css"
+
+const ModalNotAutor = () => {
+  return (
+    <div>
+      <h4>Error while saving</h4>
+      <p>To save this recipe, you need to authorize first</p>
+
+    </div>
+  )
+}
+
+export default ModalNotAutor
