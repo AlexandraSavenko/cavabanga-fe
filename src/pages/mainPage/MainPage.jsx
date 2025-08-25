5063a752b8bb21e8cc5d0c0586a6f99f4f4adf3f
Anastasiia Medved <nastya0662793393@gmail.com>
2025-08-26 01:17:02 +0200

    add SearchBox and Filters components

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import css from "./MainPage.module.css"
+import React from "react";
+import css from "./MainPage.module.css";
 
-import Logo from '../../components/logo/Logo'
-import RecipesList from '../../components/recipesList/RecipesList'
+import Logo from "../../components/logo/Logo";
+import RecipesList from "../../components/recipesList/RecipesList";
+
+import SearchBox from "../../components/SearchBox/SearchBox";
+import Filters from "../../components/Filters/Filters";
 
 const MainPage = () => {
   return (
     <div className={css.wrap}>
-        this is where recipies list will be
-<RecipesList recipeType={"own"}/>
-
+      this is where recipies list will be
+      <SearchBox onSearch={() => {}} />
+      <Filters />
+      <RecipesList recipeType={"own"} />
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
