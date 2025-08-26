323dfb9556ef3b553fc6f6534a7d75c95cd81bc7
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-26 08:59:24 +0300

    resolve conflicts

--- src/pages/addRecipePage/AddRecipePage.jsx
+++ src/pages/addRecipePage/AddRecipePage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+
+import AddRecipeForm from "../../components/AddRecipeForm/AddRecipeForm";
+import css from "./AddRecipePage.module.css"; 
 
 const AddRecipePage = () => {
   return (
-    <div>
-      Add your recipe here
-    </div>
-  )
-}
+    <div className={css.container}>
+      <h1 className={css.title}>Add Recipe</h1>
 
-export default AddRecipePage
+      <section className={css.section}>
+        <p className={css.subtitle}>General Information</p>
+      </section>
+
+      <section className={css.section}>
+        <p className={css.subtitle}>Ingredients</p>
+      </section>
+
+      <section className={css.section}>
+        <p className={css.subtitle}>Instructions</p>
+      </section>
+
+      <AddRecipeForm />
+    </div>
+  );
+};
+
+export default AddRecipePage;
+
+
