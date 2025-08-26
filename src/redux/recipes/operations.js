86115a3495a204cc04e3d09c7504dd1dfedf6d4a
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-26 22:41:27 +0300

    recipeList fetches all recipes

--- src/redux/recipes/operations.js
+++ src/redux/recipes/operations.js
@@ -1,10 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getRecipeList = createAsyncThunk("api/recires", async (_, thunkAPI) => {
+export const getRecipeList = createAsyncThunk("api/recires", async (page, thunkAPI) => {
     try {
-        const res = await axios.get("/api/recipes");
+        const res = await axios.get(`/api/recipes/?page=${page}&limit=12`);
         console.log(res.data)
+        return res.data.data
     } catch (error) {
         return thunkAPI.rejectWithValue(error.message)
     }
