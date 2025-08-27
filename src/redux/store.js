7523e1fa553f1c9a930abb9a5147c8572c016f0c
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-27 21:52:40 +0300

    merge from main

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/slice";
 import recipesReducer from "./recipes/slice";
 import filtersReducer from "./filters/slice";;
-import filtersReducer from "./filters/slice";
+
 
 export const store = configureStore({
   reducer: {
