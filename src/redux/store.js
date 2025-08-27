84bab37d6cc3a4c111a42f1b25a25e607de3f884
hanna-uler <hannauler@gmail.com>
2025-08-27 13:41:41 -0400

    add PersistGate to main.js, add user to persisted locStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 import authReducer from "./auth/slice";
 import recipesReducer from "./recipes/slice";
 import filtersReducer from "./filters/slice";
 
+const persistConfig = {
+  key: "user",
+  storage
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer)
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
     filters: filtersReducer,
     recipes: recipesReducer,
   },
 });
+
+export const persistor = persistStore(store);
