320852852815a825cb50d0669470aa393c15aa91
hanna-uler <hannauler@gmail.com>
2025-08-27 14:55:37 -0400

    add persist storage to all slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,18 +5,27 @@ import authReducer from "./auth/slice";
 import recipesReducer from "./recipes/slice";
 import filtersReducer from "./filters/slice";
 
-const persistConfig = {
-  key: "user",
+const persistAuthConfig = {
+  key: "auth",
   storage
 }
-
-const persistedAuthReducer = persistReducer(persistConfig, authReducer)
+const persistFiltersConfig = {
+  key: "filters",
+  storage
+}
+const persistRecipesConfig = {
+  key: "recipes",
+  storage
+}
+const persistedAuthReducer = persistReducer(persistAuthConfig, authReducer);
+const persistedFiltersReducer = persistReducer(persistFiltersConfig, filtersReducer);
+const persistedRecipesReducer = persistReducer(persistRecipesConfig, recipesReducer);
 
 export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
-    filters: filtersReducer,
-    recipes: recipesReducer,
+    filters: persistedFiltersReducer,
+    recipes: persistedRecipesReducer,
   },
 });
 
