77ec823b7c53b4948d58a3a08391ea03efba6de0
Sasha Savenko <159346450+AlexandraSavenko@users.noreply.github.com>
2025-08-29 13:28:03 +0300

    Alex fix br (#74)
    * Revert "Modal fail search (#73)"
    
    This reverts commit a356a5218b85296cf7793069ce3a3e4d43d022af.
    
    * bugs fixed

--- src/components/recipesList/RecipesList.jsx
+++ src/components/recipesList/RecipesList.jsx
@@ -1,46 +1,28 @@
-import React, { useState, useEffect } from 'react';
 import RecipeCard from '../recipeCard/RecipeCard.jsx';
 import LoadMoreBtn from '../loadMoreBtn/LoadMoreBtn.jsx';
 import Pagination from '../pagination/Pagination.jsx';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRecipeList } from '../../redux/recipes/operations.js';
-import { selectAllRecipes } from '../../redux/recipes/selectors.js';
 
-const RecipesList = ({ recipeType }) => {
-  const dispatch = useDispatch();
-  const allRecipes = useSelector(selectAllRecipes);
+import style from './RecipesList.module.css';
 
-  // Стан для кількості відображуваних рецептів
-  const [visibleCount, setVisibleCount] = useState(16);
+const RecipesList = ({ allRecipes, recipeType }) => {
 
-  useEffect(() => {
-    dispatch(getRecipeList(2));
-  }, [dispatch]);
-
-  const handleLoadMore = () => {
-    setVisibleCount(prev => prev + 16);
-  };
-
+  if (!Array.isArray(allRecipes)) {
+    return <p>Something went wrong</p>
+  }
   // Якщо рецептів немає
-  if (!allRecipes || allRecipes.length === 0) {
+  if (allRecipes.length === 0) {
     return <p>No recipes available</p>;
   }
-
   return (
-    <div>
-      <h2>Recipes</h2>
-      <p>{allRecipes.length} recipes</p>
-      <ul>
-        {allRecipes.slice(0, visibleCount).map(recipe => (
-          <li key={recipe.id}>
+    <div className={style.container}>
+      <ul className={style.list}>
+        {allRecipes.map(recipe => {
+          return <li key={recipe._id}>
             <RecipeCard recipe={recipe} recipeType={recipeType} />
           </li>
-        ))}
+        }
+        )}
       </ul>
-
-      {visibleCount < allRecipes.length && (
-        <LoadMoreBtn onClick={handleLoadMore} />
-      )}
       <Pagination />
     </div>
   );
