33698d13cbc5e44cca7bf20e903eadf35c4fe263
valentina2209 <valya253@ukr.net>
2025-08-29 10:16:50 +0300

    Style: add some style (#70)

--- src/components/recipesList/RecipesList.jsx
+++ src/components/recipesList/RecipesList.jsx
@@ -2,27 +2,30 @@ import RecipeCard from '../recipeCard/RecipeCard.jsx';
 import LoadMoreBtn from '../loadMoreBtn/LoadMoreBtn.jsx';
 import Pagination from '../pagination/Pagination.jsx';
 
-const RecipesList = ({allRecipes, recipeType }) => {
+import style from './RecipesList.module.css';
 
-if(!Array.isArray(allRecipes)){
-  return <p>Something went wrong</p>
-}
+const RecipesList = ({ allRecipes, recipeType }) => {
+
+  if (!Array.isArray(allRecipes)) {
+    return <p>Something went wrong</p>
+  }
   // Якщо рецептів немає
   if (allRecipes.length === 0) {
     return <p>No recipes available</p>;
   }
   return (
-    <div>
-      <h2>Recipes</h2>
+    <div className={style.container}>
+      <h2 className={style.title}>Recipes</h2>
       <p>{allRecipes.length} recipes</p>
-      <ul>
-        {allRecipes.map(recipe => 
-          { return <li key={recipe._id}>
+      <ul className={style.list}>
+        {allRecipes.map(recipe => {
+          return <li key={recipe._id}>
             <RecipeCard recipe={recipe} recipeType={recipeType} />
-          </li>}
+          </li>
+        }
         )}
       </ul>
-<Pagination />
+      <Pagination />
     </div>
   );
 };
