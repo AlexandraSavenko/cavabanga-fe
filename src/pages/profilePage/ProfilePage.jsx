2b041c8dca377a3236ebd3359cc128472851882a
Valya Vovchenko <valya253@ukr.net>
2025-08-30 18:03:06 +0300

    Fix: Corrected filter styles

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import ProfileNavigation from '../../components/profileNavigation/ProfileNavigation.jsx';
 import RecipesList from '../../components/recipesList/RecipesList.jsx';
 import LoadMoreBtn from '../../components/loadMoreBtn/LoadMoreBtn.jsx';
+import Filter from '../../components/Filters/Filters.jsx';
 
 import styles from './ProfilePage.module.css';
 import { getRecipeList, getUserFavourites } from '../../redux/recipes/operations.js';
@@ -12,23 +13,26 @@ import { selectAllRecipes, selectUserFavourites } from '../../redux/recipes/sele
 
 
 const ProfilePage = () => {
-    const { recipeType } = useParams();
-    const dispatch = useDispatch();
-    const recipes = useSelector(selectAllRecipes);
-    const favRecipes = useSelector(selectUserFavourites)
+  const { recipeType } = useParams();
+  const dispatch = useDispatch();
+  const recipes = useSelector(selectAllRecipes);
+  const favRecipes = useSelector(selectUserFavourites)
 
   useEffect(() => {
-    recipeType === "own" ? 
-dispatch(getRecipeList({type: recipeType, page: 1, perPage: 12})) :
-dispatch(getUserFavourites())
+    recipeType === "own" ?
+      dispatch(getRecipeList({ type: recipeType, page: 1, perPage: 12 })) :
+      dispatch(getUserFavourites())
   }, [dispatch, recipeType])
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>My profile</h2>
-            <ProfileNavigation />
-            <RecipesList allRecipes={recipeType === "own" ? recipes : favRecipes} recipeType={recipeType} />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>My profile</h2>
+      <ProfileNavigation />
+      <div className={styles.hideFilters}>
+        <Filter />
+      </div>
+      <RecipesList allRecipes={recipeType === "own" ? recipes : favRecipes} recipeType={recipeType} />
+    </div>
+  );
 };
 
 export default ProfilePage;
