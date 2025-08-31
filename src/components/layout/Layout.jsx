776eee03cf7b66180757d22ff8d240e1d13b7239
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-31 22:20:57 +0300

    hiro render fixed

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,13 +3,16 @@ import Footer from "../footer/Footer";
 import Header from "../header/Header";
 import css from "./Layout.module.css"
 import SearchBox from "../SearchBox/SearchBox"
+import { useLocation } from "react-router-dom";
 
 
 const Layout = ({children}) => {
+  const location = useLocation().pathname
+  const isOnMain = location === "/"
   return (
     <div id="root">
       <Header />
-      { <SearchBox /> }
+      {isOnMain && <SearchBox /> }
       <div className={`${css.contentWrap} container`}>
           {children}
       </div>
