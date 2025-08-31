fc8482c113be7ebfee06763326d4f0985fccff9e
Volodymyr Anisimov <144102084+vanisimov4@users.noreply.github.com>
2025-08-31 19:07:12 +0300

    addScrollAndCloseModalsVariations

---
+++ src/components/Scroll/Scroll.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectPage } from '../../redux/recipes/selectors';
+
+import style from './Scroll.module.css';
+
+const Scroll = ({ containerRef }) => {
+  const currentPage = useSelector(selectPage);
+
+  // автоскрол при зміні сторінки
+  useEffect(() => {
+    if (containerRef.current) {
+      const y =
+        containerRef.current.getBoundingClientRect().top + window.scrollY - 150;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
+    const updateOffset = () => {
+      if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect();
+        const offset = window.innerWidth - rect.right + 1; // 1px відступ від правої межі контейнера
+        document.documentElement.style.setProperty(
+          '--scroll-btn-right',
+          `${offset}px`
+        );
+      }
+    };
+
+    updateOffset();
+    window.addEventListener('resize', updateOffset);
+    return () => window.removeEventListener('resize', updateOffset);
+  }, [containerRef]);
+
+  const scrollToTop = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
+
+  const scrollToBottom = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+    }
+  };
+
+  return (
+    <>
+      <button
+        className={style.scrollBtn}
+        onClick={scrollToTop}
+        style={{ top: '70%' }}
+      >
+        ⬆
+      </button>
+      <button
+        className={style.scrollBtn}
+        onClick={scrollToBottom}
+        style={{ top: '80%' }}
+      >
+        ⬇
+      </button>
+    </>
+  );
+};
+
+export default Scroll;
--- src/components/recipesList/RecipesList.jsx
+++ src/components/recipesList/RecipesList.jsx
@@ -1,29 +1,36 @@
+import { useRef } from 'react';
+
 import RecipeCard from '../recipeCard/RecipeCard.jsx';
 import LoadMoreBtn from '../loadMoreBtn/LoadMoreBtn.jsx';
 import Pagination from '../pagination/Pagination.jsx';
+import Scroll from '../Scroll/Scroll.jsx';
 
 import style from './RecipesList.module.css';
 
 const RecipesList = ({ allRecipes, recipeType }) => {
+  const listRef = useRef(null); // посилання на контейнер для скролу
 
   if (!Array.isArray(allRecipes)) {
-    return <p>Something went wrong</p>
+    return <p>Something went wrong</p>;
   }
   // Якщо рецептів немає
   if (allRecipes.length === 0) {
     return <p>No recipes available</p>;
   }
+
   return (
-    <div className={style.container}>
+    <div ref={listRef} className={style.container}>
       <ul className={style.recipesList}>
         {allRecipes.map(recipe => {
-          return <li className={style.item} key={recipe._id}>
-            <RecipeCard recipe={recipe} recipeType={recipeType} />
-          </li>
-        }
-        )}
+          return (
+            <li className={style.item} key={recipe._id}>
+              <RecipeCard recipe={recipe} recipeType={recipeType} />
+            </li>
+          );
+        })}
       </ul>
       <Pagination />
+      <Scroll containerRef={listRef} />
     </div>
   );
 };
