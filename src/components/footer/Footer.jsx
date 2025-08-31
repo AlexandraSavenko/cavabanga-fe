cb2eca26e6b9e96acfe8c5265fd2fcaf3d35daee
Sasha Savenko <sashasavenko3@gmail.com>
2025-08-31 08:51:07 +0300

    footer modal changed to common modal but no styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import css from "./Footer.module.css";
 import Logo from "../logo/Logo";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import ProtectedLink from "../protectedLink/ProtectedLink";
-import ModalNotAutor from "../modalNotAutor/ModalNotAutor";
+import ModalErrorCommon from "../ModalErrorCommon/ModalErrorCommon";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 const Footer = () => {
   const [showModal, setShowModal] = useState(false);
-  const user = null;
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+  const user = useSelector(selectIsLoggedIn);
   return (
     <div className={css.wrap}>
       <div className={`${css.footer} container`}>
@@ -15,21 +20,41 @@ const Footer = () => {
         <p>© 2025 CookingCompanion. All rights reserved.</p>
         <nav className={css.footerNav}>
           <NavLink
-            className={({ isActive }) => isActive && css.active}
+            className={({ isActive }) => isActive ? css.active : ""}
             to={"/"}
           >
             Recipes
           </NavLink>
           <ProtectedLink
             to="/profile/own"
-            isLoggedIn={user !== null}
+            isLoggedIn={user}
             openLoginModal={() => setShowModal(true)}
           >
             Account
           </ProtectedLink>
         </nav>
       </div>
-      {showModal && <ModalNotAutor modalOpen={setShowModal}/> }
+      <ModalErrorCommon isopen={showModal} onClose={handleModalClose}>
+        <p className={css.text}>
+          You need to log in to have access ot your account.
+        </p>
+        <div className={css.linkWrap}>
+          <Link
+            className={css.login}
+            onClick={handleModalClose}
+            to={"/auth/login"}
+          >
+            Log in
+          </Link>
+          <Link
+            className={css.register}
+            onClick={handleModalClose}
+            to={"/auth/register"}
+          >
+            Register
+          </Link>
+        </div>
+      </ModalErrorCommon>
     </div>
   );
 };
