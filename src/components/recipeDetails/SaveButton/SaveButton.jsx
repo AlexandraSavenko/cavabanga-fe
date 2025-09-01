4b5d9d0f761cfc9fe133482444ca6379d4e5cdf0
Yulia <derevyanko.julia@gmail.com>
2025-09-01 13:30:54 +0300

    Refactor RecipeSection to display preparation steps as a list and update SaveButton styles for better UI consistency

--- src/components/recipeDetails/SaveButton/SaveButton.jsx
+++ src/components/recipeDetails/SaveButton/SaveButton.jsx
@@ -2,13 +2,18 @@ import { useLocation } from "react-router-dom";
 import css from "./SaveButton.module.css";
 
 export default function SaveButton({ onClick, isFavorite }) {
-  const location = useLocation().pathname
-  const isOnMain = !location.includes("recipe")
+  const location = useLocation().pathname;
+  const isOnMain = !location.includes("recipe");
   return (
-    <button className={`${isOnMain ? css.miniBtn : ""} ${css.saveButton}`} onClick={onClick}>
+    <button
+      className={`${isOnMain ? css.miniBtn : ""} ${css.saveButton}`}
+      onClick={onClick}
+    >
       {isOnMain ? "" : isFavorite ? "Unsave" : "Save"}
       <svg
-        className={`${css.icon} ${isFavorite ? css.active : ""}`}
+        className={`${isOnMain ? css.miniIcon : css.icon} ${
+          isFavorite && isOnMain ? css.activeMin : isFavorite ? css.active : ""
+        }`}
         width="24"
         height="24"
       >
