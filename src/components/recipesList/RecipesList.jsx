fdb1548d25ced5e3c117c563f0906e28e7ac6023
Hanna Uler <hannauler@gmail.com>
2025-08-31 23:52:50 -0400

    Add NoMatchFound comp and condition for Pagination rendering (#108)
    * add NoMatchFound comp
    
    * fix reset dispatch, add NoMatch comp to RecipesList
    
    * add css for NoMatchFound
    
    * add condit. pagination render

--- src/components/recipesList/RecipesList.jsx
+++ src/components/recipesList/RecipesList.jsx
@@ -6,6 +6,7 @@ import Pagination from '../pagination/Pagination.jsx';
 import Scroll from '../Scroll/Scroll.jsx';
 
 import style from './RecipesList.module.css';
+import NoMatchFound from '../NoMatchFound/NoMatchFound.jsx';
 
 const RecipesList = ({ allRecipes, recipeType }) => {
   const listRef = useRef(null); // посилання на контейнер для скролу
@@ -13,11 +14,7 @@ const RecipesList = ({ allRecipes, recipeType }) => {
   if (!Array.isArray(allRecipes)) {
     return <p>Something went wrong</p>;
   }
-  // Якщо рецептів немає
-  if (allRecipes.length === 0) {
-    return <p>No recipes available</p>;
-  }
-
+ 
   return (
     <div ref={listRef} className={style.container}>
       <ul className={style.recipesList}>
@@ -29,7 +26,8 @@ const RecipesList = ({ allRecipes, recipeType }) => {
           );
         })}
       </ul>
-      <Pagination />
+      {allRecipes.length > 0 && <Pagination />}
+      {allRecipes.length === 0 && <NoMatchFound/>}
       <Scroll containerRef={listRef} />
     </div>
   );
